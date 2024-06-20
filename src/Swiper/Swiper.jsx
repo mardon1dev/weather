608f7e218295557dded5163524efc566126db614
mardon1dev <dusbekovmardonbek@gmail.com>
2024-06-21 01:02:30 +0500

    Final

--- src/Swiper/Swiper.jsx
+++ src/Swiper/Swiper.jsx
@@ -29,7 +29,7 @@ const hourlyForecast = () => {
                     <SwiperSlide key={index}>
                         <div key={index} className="flex flex-col items-center justify-between gap-3 bg-[#373636] min-w-[80px] py-4 rounded-[30px] min-h-[200px] h-full">
                             <span className='text font-bold'>{item.time.split(" ").slice(1,2)}</span>
-                            <img className='hourly-img' src={`https:${item.condition.icon}`} alt="Condition"  width={100} height={50}/>
+                            <img className='hourly-img' src={`https:${item.condition.icon}`} alt="Condition"  width={100} height={100}/>
                             <span>{item.temp_c}°C</span>
                             <span className='text-center'>{item.condition.text}</span>
                         </div>
@@ -52,7 +52,7 @@ const hourlyForecast = () => {
                     <SwiperSlide key={index}>
                         <div key={index} className="flex flex-col items-center justify-between gap-3 bg-[#373636] min-w-[80px] py-4 rounded-[30px] min-h-[200px] h-full">
                             <span className='text font-bold'>{item.time.split(" ").slice(1,2)}</span>
-                            <img className='hourly-img' src={`https:${item.condition.icon}`} alt="Condition"  width={100} height={50}/>
+                            <img className='hourly-img' src={`https:${item.condition.icon}`} alt="Condition"  width={100} height={100}/>
                             <span>{item.temp_c}°C</span>
                             <span className='text-center'>{item.condition.text}</span>
                         </div>
